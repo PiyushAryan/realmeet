cf594a7ad28824eea822ca1e3c59436884a99e15
Piyush Aryan <piyusharyan81@gmail.com>
2024-12-24 01:09:43 +0530

    feat:add the sidebar in editor page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,10 +1,23 @@
-import CodeEditor from '../components/CodeEditor/CodeEditor'
+import Workspace from '../components/Workspace/Workspace'
+import Sidebar from '../components/Sidebar/Sidebar'
+import Split from 'react-split'
 
 function Editor() {
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <div className='text-4xl font-bold px-4'>Editor</div>
-            <CodeEditor />
+        <div className="flex h-screen">
+            <Split
+                sizes={[30, 70]} 
+                gutterSize={8} // Space between the two panes
+                minSize={200}
+                className="flex w-full" 
+            >
+                <div className="bg-gray-800 h-full">
+                    <Sidebar />
+                </div>
+                <div className="bg-gray-100 h-full">
+                    <Workspace />
+                </div>
+            </Split>
         </div>
     )
 }
