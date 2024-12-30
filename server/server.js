2fc8cbc72b068baa32634f054c664b5c40fe2527
Piyush Aryan <piyusharyan81@gmail.com>
2024-12-31 03:40:40 +0530

    feat:add sockets inside the server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
