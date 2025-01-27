483b3e95623f5c8173a1920422751b2602a416b5
Piyush Aryan <piyusharyan81@gmail.com>
2025-01-28 02:08:42 +0530

    server url update in env

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://realmeet-eam6.onrender.com',
+        ws: true
   },
   plugins: [react()],
 })
