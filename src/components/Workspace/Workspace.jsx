0556d048d4a9fb15a0d7feb701574fcaa98be8c0
Piyush Aryan <piyusharyan81@gmail.com>
2025-04-25 02:41:21 +0530

    feat:add judge0 compiler api

--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Codemirror from 'codemirror';
+import axios from 'axios';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 import 'codemirror/mode/javascript/javascript';
@@ -8,6 +9,7 @@ import 'codemirror/addon/edit/closebrackets';
 
 function Workspace({ socketRef, roomId }) {
     const editorRef = useRef(null);
+    const [output, setOutput] = useState('');
 
     useEffect(() => {
         async function connect() {
@@ -60,7 +62,64 @@ function Workspace({ socketRef, roomId }) {
         };
     }, [socketRef, roomId]);
 
-    return <textarea id="realtimeEditor"></textarea>;
+    const runCode = async () => {
+        const rawCode = editorRef.current.getValue();
+        const encodedCode = btoa(rawCode); // base64 encode  encodeing the code part using btoa
+        const stdin = btoa("Judge0");
+    
+        setOutput('Compiling...');
+    
+        const options = {
+            method: 'POST',
+            url: 'https://judge0-ce.p.rapidapi.com/submissions',
+            params: {
+                base64_encoded: 'true',
+                wait: 'true',  
+                fields: '*'
+            },
+            headers: {
+                'x-rapidapi-key': '739bd76508msh59d87ad81cefe3dp1c1d57jsnaead8c2097b7',
+                'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+                'Content-Type': 'application/json'
+            },
+            data: {
+                language_id: 93,      // JS lang code
+                source_code: encodedCode,
+                stdin
+            }
+        };
+    
+        try {
+            const response = await axios.request(options);
+            const { stdout, stderr, compile_output, message } = response.data;
+    
+            const finalOutput =
+                stdout ? atob(stdout) :   //decoding the output part using atob
+                stderr ? atob(stderr) :
+                compile_output ? atob(compile_output) :
+                message ? atob(message) :
+                "No output";
+    
+            setOutput(finalOutput);
+        } catch (error) {
+            console.error("Error executing code:", error.message);
+            setOutput("Execution failed.");
+        }
+    };
+    
+
+    return (
+        <>
+        <div>
+                <button className="bg-violet-500 rounded-sm border border-purple-800 hover:border-white hover:text-white px-3 py-3" onClick={runCode}>Run Code</button>
+                <code>{output}</code>
+            </div>
+            <textarea id="realtimeEditor"></textarea>
+            
+        </>
+
+    );
+
 }
 
 export default Workspace;
